4df62276d5f0cb61b62fd65fe20d5ca42e47c6ee
CzarSlayer <moradabahni@gmail.com>
2024-08-12 15:19:53 +0100

    first commit

---
+++ mariadb/init-db.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Start MariaDB service
+service mariadb start
+
+# Wait for MariaDB to be ready
+# while ! mysqladmin ping -h localhost --silent; do
+    echo "Waiting for MariaDB to be ready..."
+    sleep 5
+# done
+
+db_name="walo"
+db_user="walo"
+db_pass="walo"
+
+# Initialize the database
+
+mariadb -e "CREATE DATABASE IF NOT EXISTS $db_name;"
+mariadb -e "CREATE USER IF NOT EXISTS '$db_user'@'%' IDENTIFIED BY '$db_pass';"
+mariadb -e "GRANT ALL PRIVILEGES ON $db_name.* TO '$db_user'@'%';"
+mariadb -e "FLUSH PRIVILEGES;"
+
+echo "Database initialized"
+
+service mariadb stop
+
+mysqld --bind-address=0.0.0.0
